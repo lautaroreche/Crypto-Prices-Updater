cb43e53e3d0f062c34add4da657848d4ad778a78
Lautaro Reche <97992228+lautaroreche@users.noreply.github.com>
2023-07-08 17:27:57 -0300

    Update actualizarPrecios.py
    Reduce dependencias y optimiza en cuanto a velocidad de obtención de datos

--- actualizarPrecios.py
+++ actualizarPrecios.py
@@ -1,85 +1,62 @@
-import json
-from collections import defaultdict
 import requests
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import time
-from datetime import datetime
+import pandas as pd
 
 
 
 def actualizarPrecios():
-    crypto1 = defaultdict(dict)
-    crypto2 = defaultdict(dict)
+    print('Inicio del script')  
 
-    print('------------------------------------------------')  
-    print('----- Comenzó la actualización a las',datetime.now().strftime('%H:%M'),'-----')
+    preciosActuales = []
+    variaciones = []
+    otrosElementos = ["", "Seguimiento", "USDT", "Token"]
 
-    numero = 2
+    # Determina APIs de Google y autoriza credenciales
     scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
     creds = ServiceAccountCredentials.from_json_keyfile_name("credenciales.json", scope)
     client = gspread.authorize(creds)
-
-    #Crypto general data
-    api1 = requests.get("https://www.binance.com/bapi/asset/v2/public/asset-service/product/get-products?includeEtf=true").json()['data']
-    #24hr Ticker Price Change Statistics
-    api2 = requests.get("https://www.binance.com/api/v3/ticker/24hr").json()
     
-    # Abre la pestaña Datos del Google Worksheet Crypto
-    worksheet = client.open('Crypto').worksheet("Datos")
-    # Obtiene todas las claves de los tokens del sheet
-    monedas = worksheet.col_values(2) 
-    # Elimina duplicados
-    monedas = list(set(monedas)) 
-    # Ordena la lista
-    monedas.sort() 
+    # Abre la pestaña Datos del Google Worksheet, obtiene las claves de los tokens, elimina duplicados y ordena la lista
+    worksheet = client.open('Inv').worksheet("Datos")
+    monedasEnTenencia = worksheet.col_values(1)
+    monedasASeguir = worksheet.col_values(22)
+    monedas = monedasEnTenencia + monedasASeguir
+    monedas = list(set(monedas))
+    monedas.sort()
 
-    # Elimina de la lista de monesas lo que nos sean claves de tokens
+    for elemento in otrosElementos:
+        # Elimina elementos de la pestaña que no son tokens
+        monedas.remove(elemento)
+
     for moneda in monedas:
-        if moneda.isupper() == False or len(moneda) > 5 or moneda in ['Token','Atento compras','']:
+        if moneda.isupper():
+            # Guarda los valores necesarios de cada token en dos listas si es que está listado en Binance
+            par = moneda + "USDT"
+            url = f"https://www.binance.com/api/v3/ticker/24hr?symbol={par}"
+            response = requests.get(url).json()
             try:
-                monedas.remove(moneda)
+                preciosActuales.append(float(response.get('lastPrice')))
+                variaciones.append(float(response.get('priceChangePercent')))
             except:
-                continue
+                preciosActuales.append('N/A')
+                variaciones.append('N/A')
 
-    # Toma los valores de cada api
-    for value in api1:
-        if value['s'].endswith("USDT"):
-            crypto1[value['b']] = {"CURRENT": value['c']}
-    for value in api2:
-        if value['symbol'].endswith("USDT"):
-            crypto2[value['symbol'][:-4]] = {"RESULTADO24HORAS": value['priceChangePercent'], "MAXIMO24HORAS": value['highPrice'], "MINIMO24HORAS": value['lowPrice']}
+    # Genera un DataFrame con los datos de cada token     
+    data = {
+        'Token': monedas,
+        'Precio actual': preciosActuales,
+        'Variación 24h': variaciones
+    }
+    df = pd.DataFrame(data)
+    matriz = df.values.tolist()
 
-    # Genera los json
-    with open("api1.json", "w") as api1:
-        api1.write(json.dumps(crypto1)) 
-    with open("api2.json", "w") as api2:
-        api2.write(json.dumps(crypto2))
+    # Abre la pestaña Detalle del Google Worksheet y postea la matriz
+    worksheet = client.open('Inv').worksheet('Detalle')
+    worksheet.update('A2',matriz)
 
-    # Carga los json para que podamos consultar sus datos
-    with open('api1.json') as api1:
-        api1 = json.load(api1) 
-    with open('api2.json') as api2:
-        api2 = json.load(api2)
+    print('Fin del script')
 
-    worksheet = client.open('Crypto').worksheet("Detalle")
-
-    for moneda in monedas:
-        try:
-            worksheet.update_acell('A'+str(numero),moneda)
-            worksheet.update_acell('B'+str(numero),api1[moneda].get('CURRENT','Error al obtener el precio'))
-            worksheet.update_acell('C'+str(numero),api2[moneda].get('RESULTADO24HORAS','Error al obtener el precio'))
-            numero += 1
-            print('Actualizó ' + moneda + ' ya que la encontró en Binance')
-        except:
-            worksheet.update_acell('B'+str(numero),'N/A')
-            worksheet.update_acell('C'+str(numero),'N/A')
-            numero += 1
-            print('No se actualizó ' + moneda + ' ya que no la encontró en Binance')
-        time.sleep(2)
   
-    print('----- Se actualizó la planilla a las',datetime.now().strftime('%H:%M'),'-----')
-    print('------------------------------------------------')  
-
 
 actualizarPrecios()
